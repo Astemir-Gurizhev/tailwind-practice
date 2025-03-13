ecc02f6bc8fcc871ad66b912e351b5bed2fef7bf
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-13 08:52:18 +0300

    Effects and filters. Creating a modal window.

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import './App.css'
 
 export function App() {
 	return (
-		<div className='container mx-auto'>
-
+		<div className='container mx-auto relative'>
 			<h1 className='text-5xl font-bold text-orange-400 text-center mt-32 '>
 				hello, tailwind
 			</h1>
@@ -12,20 +11,29 @@ export function App() {
 				click me
 			</button>
 
-			<div className='bg-blue-400 mt-7 w-24 h-32'>
-				width & height
-			</div>
+			<div className='bg-blue-400 mt-7 w-24 h-32'>width & height</div>
 
 			<div className='flex justify-center items-center gap-5 mt-3'>
-				<div className='bg-amber-400 w-20 h-20 '/>
-				<div className='bg-amber-400 w-20 h-20 '/>
+				<div className='bg-amber-400 w-20 h-20 ' />
+				<div className='bg-amber-400 w-20 h-20 ' />
 			</div>
 
 			<div className='grid grid-cols-2 gap-5 mt-3'>
-				<div className='bg-amber-200 h-20 '/>
-				<div className='bg-amber-200 h-20 '/>
+				<div className='bg-amber-200 h-20 ' />
+				<div className='bg-amber-200 h-20 ' />
 			</div>
 
+			<div className='fixed inset-0 bg-black/80 backdrop-blur-md flex items-center justify-center'>
+				<div className='bg-white/80 text-black rounded w-1/2 p-5'>
+					<h1 className='text-xl font-semibold'>modal window</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+						sed architecto illo suscipit, dolores expedita voluptates quod?
+						Natus, nobis exercitationem reprehenderit non soluta vero culpa
+						nulla, quibusdam dolores expedita fuga.
+					</p>
+				</div>
+			</div>
 		</div>
 	)
 }
