a684d8c294b6cb779434218c1ac6edf371b5e52b
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-13 08:04:29 +0300

    Margins (margin, padding, borders)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import './App.css'
 export function App() {
 	return (
 		<>
-			<h1 className='text-5xl font-bold text-orange-400'>hello, tailwind</h1>
+			<h1 className='text-5xl font-bold text-orange-400 text-center mt-32 '>
+				hello, tailwind
+			</h1>
+
+			<button className='rounded-3xl bg-orange-500 mt-10 px-7 py-2 cursor-pointer border border-#fff/50 border-solid'>
+				click me
+			</button>
 		</>
 	)
 }
