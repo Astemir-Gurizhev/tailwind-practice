df6d6ce5f526b81a856c9406183c72ef5ce28eaa
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-03-13 08:09:56 +0300

    Width&Height

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 export function App() {
 	return (
 		<>
+
 			<h1 className='text-5xl font-bold text-orange-400 text-center mt-32 '>
 				hello, tailwind
 			</h1>
@@ -10,6 +11,11 @@ export function App() {
 			<button className='rounded-3xl bg-orange-500 mt-10 px-7 py-2 cursor-pointer border border-#fff/50 border-solid hover:bg-orange-600'>
 				click me
 			</button>
+
+			<div className='bg-blue-400 mt-7 w-24 h-32'>
+				width & height
+			</div>
+
 		</>
 	)
 }
